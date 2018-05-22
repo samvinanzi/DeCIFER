ccec396eee80126b67b2345cf7dabbc9cd0d88c1
samvinanzi <samuelevinanzi@gmail.com>
2018-05-22 14:04:02 +0100

    Fixed Skeleton image generation and display: computations are now correctly performed on the 2D dataset. Renamed the visualization functions.

--- Skeleton.py
+++ Skeleton.py
@@ -35,22 +35,19 @@ op = PyOP.OpenPose(net_pose_size, net_face_hands_size, output_size, model, model
 
 class Skeleton:
     def __init__(self, image, robot, id=0):
-        # Member initialization
         self.origin = image.copy()
         self.keypoints = {}
+        self.keypoints_2d = {}
         self.img = None
         self.id = id
-        #self.robot = robot
         # Performs computations
         self.prepare(robot)
 
     # Prepares the data for usage and clustering
     def prepare(self, robot):
         self.get_keypoints(robot)
-        self.generate_image()
         #self.convert_to_cartesian() # Not needed, SFM already converts pixel to cartesian
         self.cippitelli_norm()
-        #self.plot(save=False)
 
     # Retrieves the skeletal keypoints
     def get_keypoints(self, robot):
@@ -76,6 +73,20 @@ class Skeleton:
         keypoints = np.vstack([keypoints, newpoint])
         # Removes the "confidence" column
         keypoints = np.delete(keypoints, 2, axis=1)
+        # Saves the 2D pixel representation of the keypoints
+        self.keypoints_2d = {
+            "Head": Keypoint(keypoints[0][0], keypoints[0][1]),
+            "Neck": Keypoint(keypoints[1][0], keypoints[1][1]),
+            "RElbow": Keypoint(keypoints[2][0], keypoints[2][1]),
+            "RWrist": Keypoint(keypoints[3][0], keypoints[3][1]),
+            "LElbow": Keypoint(keypoints[4][0], keypoints[4][1]),
+            "LWrist": Keypoint(keypoints[5][0], keypoints[5][1]),
+            "RKnee": Keypoint(keypoints[6][0], keypoints[6][1]),
+            "RAnkle": Keypoint(keypoints[7][0], keypoints[7][1]),
+            "LKnee": Keypoint(keypoints[8][0], keypoints[8][1]),
+            "LAnkle": Keypoint(keypoints[9][0], keypoints[9][1]),
+            "Torso": Keypoint(keypoints[10][0], keypoints[10][1])
+        }
         # Converts the keypoints to 3D representation
         keypoints3d = robot.request_3d_points(keypoints.tolist())
         #keypoints3d = np.append(keypoints, np.zeros((11, 1)), axis=1)   # 2D degeneration, for testing
@@ -93,21 +104,33 @@ class Skeleton:
             "LAnkle": Keypoint(keypoints3d[9][0], keypoints3d[9][1], keypoints3d[9][2]),
             "Torso": Keypoint(keypoints3d[10][0], keypoints3d[10][1], keypoints3d[10][2])
         }
+        # Generates the image based on the 2D pixel keypoints
+        # Done after the request of 3D coordinates to avoid time delays
+        self.generate_image()
 
     # Computes the missing keypoint names
-    def get_missing_keypoints(self):
+    def get_missing_keypoints(self, apply_on_2d=False):
+        if apply_on_2d:
+            items = self.keypoints_2d.items()
+        else:
+            items = self.keypoints.items()
         output = []
-        for name, keypoint in self.keypoints.items():
+        for name, keypoint in items:
             if keypoint.is_empty():
                 output.append(name)
         return output
 
     # Returns the non-missing keypoint dictionary
-    def nonmissing_keypoints(self):
-        missing_keypoints = self.get_missing_keypoints()
-        return {key: self.keypoints[key] for key in self.keypoints if key not in missing_keypoints}
+    def nonmissing_keypoints(self, apply_to_2d=False):
+        if apply_to_2d:
+            keypoints = self.keypoints_2d
+        else:
+            keypoints = self.keypoints
+        missing_keypoints = self.get_missing_keypoints(apply_to_2d)
+        return {key: keypoints[key] for key in keypoints if key not in missing_keypoints}
 
     # Converts the dictionary of Keypoints into an ordered numpy array
+    # To use it on the 2D keypoint set, just pass it as an argument
     def keypoints_to_array(self, keypoints=None):
         # This condition enables the computation even on other, external or temp keypoint arrays
         if keypoints is None:
@@ -121,9 +144,10 @@ class Skeleton:
         return np.array(output)
 
     # Computes the minimum 2D bounding box around the detected skeletal keypoints
+    # Works on the 2D keypoints dataset
     def bounding_box(self, padding=0.2):
         # Remove from the computation the missing keypoints
-        temp_keypoints = self.nonmissing_keypoints()
+        temp_keypoints = self.nonmissing_keypoints(apply_to_2d=True)
         # Converts the new keypoints to numpy array
         temp_keypoints = self.keypoints_to_array(temp_keypoints)
         # Min
@@ -148,47 +172,17 @@ class Skeleton:
         min_y = max(0, min_y)
         return [int(min_x), int(min_y), int(min_x) + int(width), int(min_y) + int(height)]
 
-    # Crops and displays the skeleton, with or without the background generating image
-    def show(self, color=(255, 255, 255), background=False, save=False, savename="skeleton"):
-        if self.img is not None:
-            print("Warning! show() cannot be used for transformed and normalized images. Use display() instead.")
-            return None
-        # Sets the background
-        if background:
-            image = self.origin.copy()
-        else:
-            height, width, channels = self.origin.shape
-            image = np.zeros((height, width, channels), np.uint8)
-        box = self.bounding_box()     # Fetches the bounding box dimensions
-        # Iterates for each non-missing point
-        for name, keypoint in self.nonmissing_keypoints().items():
-            cv2.circle(image, (int(keypoint.x), int(keypoint.y)), 3, color, 5)
-            cv2.putText(image, name, (int(keypoint.x), int(keypoint.y)), cv2.FONT_HERSHEY_SIMPLEX, 1, color)
-        # Crops the image
-        roi = image[box[1]:box[3], box[0]:box[2]]
-        if save:
-            cv2.imwrite("img/test/" + savename + ".jpg", roi)
-        cv2.imshow("Skeletal ROI", roi)
-        cv2.waitKey(0)
-        # Returns the image, in case some other component needs it
-        return roi
-
     # Generates a B/W image of the skeleton and stores it
     def generate_image(self):
         # Sets the background
         self.img = self.origin.copy()
         box = self.bounding_box()  # Fetches the bounding box dimensions
         # Iterates for each non-missing point
-        for name, keypoint in self.nonmissing_keypoints().items():
+        for name, keypoint in self.nonmissing_keypoints(apply_to_2d=True).items():
             cv2.circle(self.img, (int(keypoint.x), int(keypoint.y)), 1, (0, 255, 255), 5)
             # cv2.putText(image, name, (int(keypoint.x), int(keypoint.y)), cv2.FONT_HERSHEY_SIMPLEX, 1, color)
         # Crops the image
         self.img = self.img[box[1]:box[3], box[0]:box[2]].copy()
-
-    # Visualizes the skeleton image. To be used when the keypoints have already been normalized
-    def display(self):
-        cv2.imshow("im", self.img)
-        cv2.waitKey(0)
 
     # Cippitelli normalization
     def cippitelli_norm(self):
@@ -203,9 +197,42 @@ class Skeleton:
             # Substitute this point to the original one
             self.keypoints[name] = di
 
-    # Plots the data points
-    def plot(self, save=False):
+    # Returns a row array (1x30) of features for this skeleton as a dataset example
+    def as_feature(self):
         array = self.keypoints_to_array()
+        # Deletes the final row corresponding to Torso.x and Torso.y (they are always zero)
+        array = array[:-1, :]
+        return np.array(array).ravel()
+
+    # Converts pixel coordinates to cartesian
+    def convert_to_cartesian(self):
+        # Work on a copy of the original data
+        kps = self.nonmissing_keypoints(apply_to_2d=True)
+        height, width, _ = self.origin.shape
+        for name, kp in kps.items():
+            kp.x = kp.x - width / 2
+            kp.y = -kp.y + height / 2
+            self.keypoints[name] = kp
+
+    # Converts cartesian coordinates to pixel
+    def convert_to_pixel(self):
+        # Work on a copy of the original data
+        kps = self.nonmissing_keypoints(apply_to_2d=True)
+        height, width, _ = self.origin.shape
+        for name, kp in kps.items():
+            kp.x = kp.x + width/2
+            kp.y = -kp.y + height/2
+            self.keypoints[name] = kp
+
+    # ---- DISPLAY FUNCTIONS ----
+
+    # Plots the data points
+    def plot_keypoints(self, save=False, apply_to_2d=False):
+        if apply_to_2d:
+            print("[WARNING] 2D keypoints have not been converted to cartesian space.")
+            array = self.keypoints_to_array(self.keypoints_2d)
+        else:
+            array = self.keypoints_to_array()
         x = array[:, 0]
         y = array[:, 1]
         z = array[:, 2]
@@ -224,32 +251,32 @@ class Skeleton:
             plt.savefig("plot.png")
         plt.show()
 
-    # Converts pixel coordinates to cartesian
-    def convert_to_cartesian(self):
-        # Work on a copy of the original data
-        kps = self.nonmissing_keypoints()
-        height, width, _ = self.origin.shape
-        for name, kp in kps.items():
-            kp.x = kp.x - width / 2
-            kp.y = -kp.y + height / 2
-            self.keypoints[name] = kp
+    # Quickly displays the associated image for the skeleton.
+    def display_fast(self):
+        cv2.imshow("im", self.img)
+        cv2.waitKey(0)
 
-    # Converts cartesian coordinates to pixel
-    def convert_to_pixel(self):
-        # Work on a copy of the original data
-        kps = self.nonmissing_keypoints()
-        height, width, _ = self.origin.shape
-        for name, kp in kps.items():
-            kp.x = kp.x + width/2
-            kp.y = -kp.y + height/2
-            self.keypoints[name] = kp
-
-    # Returns a row array (1x30) of features for this skeleton as a dataset example
-    def as_feature(self):
-        array = self.keypoints_to_array()
-        # Deletes the final row corresponding to Torso.x and Torso.y (they are always zero)
-        array = array[:-1, :]
-        return np.array(array).ravel()
+    # Image processing and visualization. Crops and displays the skeleton, with or without the background image
+    def display(self, color=(255, 255, 255), background=False, save=False, savename="skeleton"):
+        # Sets the background
+        if background:
+            image = self.origin.copy()
+        else:
+            height, width, channels = self.origin.shape
+            image = np.zeros((height, width, channels), np.uint8)
+        box = self.bounding_box()  # Fetches the bounding box dimensions
+        # Iterates for each non-missing point
+        for name, keypoint in self.nonmissing_keypoints(apply_to_2d=True).items():
+            cv2.circle(image, (int(keypoint.x), int(keypoint.y)), 3, color, 5)
+            cv2.putText(image, name, (int(keypoint.x), int(keypoint.y)), cv2.FONT_HERSHEY_SIMPLEX, 1, color)
+        # Crops the image
+        roi = image[box[1]:box[3], box[0]:box[2]]
+        if save:
+            cv2.imwrite("img/test/" + savename + ".jpg", roi)
+        cv2.imshow("Skeletal ROI", roi)
+        cv2.waitKey(0)
+        # Returns the image, in case some other component needs it
+        return roi
 
 
 # Custom exception
