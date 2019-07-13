6daef0b3318bd5165466abfd18722bce2601a8eb
samvinanzi <samuelevinanzi@gmail.com>
2019-07-13 13:56:41 +0100

    Color sequence validation for trust estimation purposes.

--- colors.py
+++ colors.py
@@ -7,11 +7,13 @@ Color detection and blob analysis.
 
 import cv2
 import numpy as np
+import re
 
 
 class ColorObserver:
     def __init__(self):
-        self.processed_img = None
+        self.latest_image = None
+        self.latest_sequence = None
         # Known colors and their ranges (lower and upper)
         self.colors = {
             'blue': [
@@ -71,12 +73,29 @@ class ColorObserver:
         for color, coords in centroids.items():
             cv2.circle(img, (coords[0], coords[1]), 10, (0, 0, 0), -1)
             cv2.putText(img, color, (coords[0]-50, coords[1]-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), cv2.LINE_4)
-        self.processed_img = img
+        self.latest_image= img
         # Sort the centroids to determine the color sequence
         sequence = [x[0].upper() for x in sorted(centroids.items(), key=lambda item: item[1])]
+        self.latest_sequence = sequence
         return sequence
 
+    # Verifies if the cubes are aligned in a valid sequence
+    def validate_sequence(self, sequence=None):
+        if not sequence:
+            sequence = self.latest_sequence
+        # Converts the list of strings in a string code (e.g. 'BORG')
+        string_sequence = ""
+        for element in sequence:
+            string_sequence += element[0]
+        # Tries to validate the string code against a regexp
+        p = re.compile('([B|O][G|R][B|O][G|R])|([G|R][B|O][G|R][B|O])')
+        m = p.match(string_sequence)
+        if m:
+            return True
+        else:
+            return False
+
     def display(self):
-        cv2.imshow("Processed image", self.processed_img)
+        cv2.imshow("Processed image", self.latest_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
